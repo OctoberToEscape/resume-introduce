a1f1d431a4c83b177ae8c5b9d74880b5c42e5ec2
何琪 <916615912@qq.com>
2020-09-17 13:07:02 +0800

    制作

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+module.exports = {
+    outputDir: "dist",
+    assetsDir: "static",
+    lintOnSave: false,
+    publicPath: "/",
+    configureWebpack: () => {
+        return {
+            devtool: "source-map",
+            devServer: {
+                // proxy: {},
+                // public: "127.0.0.1:8901", // For docker host
+                watchOptions: {
+                    poll: true,
+                    ignored: /(node_modules|dist|build|vendor|Docker|tests|backup|\.lock|\.git)/,
+                },
+                disableHostCheck: true,
+                historyApiFallback: true,
+            },
+        };
+    },
+};
